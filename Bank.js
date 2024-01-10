108cbb61853080a438884cc872951ced3402f472
Louvel-Marvin <155963514+Louvel-Marvin@users.noreply.github.com>
2024-01-10 13:12:19 +0100

    Ajout d'un sysème de compte

--- Bank.js
+++ Bank.js
@@ -1,37 +1,104 @@
 export class Bank {
-  name = "";
-  constructor(name) {
-    this.name = name;
+  constructor() {
+    this.clients = [];
   }
 
-  clients = [];
+  addClient(client) {
+    this.clients.push(client);
+  }
 
-  addClient(client) {}
+  removeClient(name) {
+    this.clients = this.clients.filter(client => client.name !== name);
+  }
 
-  displayClient(name) {}
+  displayClient(name) {
+    const client = this.clients.find(client => client.name === name);
+    if (client) {
+      console.log(`Nom : ${client.name}, Dépositaire de: ${client.balance} €`);
+    } else {
+      console.log(`Le client nommé ${name} n'a pas été trouvé.`);
+    }
+  }
 
-  displayAllClients() {}
+  displayAllClients() {
+    this.clients.forEach(client => {
+      console.log(`Nom du client: ${client.name}, Dépositaire de: ${client.balance}`);
+    });
+  }
 
-  displayRichestClient(top = 10) {}
+  displayRichestClient(top = 5) {
+    const sortedClients = this.clients.slice().sort((a, b) => b.balance - a.balance);
+    const topClients = sortedClients.slice(0, top);
+    topClients.forEach(client => {
+      console.log(`Nom du client: ${client.name}, Dépositaire de: ${client.balance}€`);
+    });
+  }
+}
 
-  removeClient(name) {}
+export class CommonAccount {
+  constructor(accountName, client1, client2) {
+    this.accountName = accountName;
+    this.clients = [client1, client2];
+    this.balance = client1.balance + client2.balance;
+  }
+
+  deposit(amount) {
+    if (amount > 0) {
+      this.balance += amount;
+    }
+  }
+
+  withdraw(amount) {
+    if (amount > 0 && this.balance >= amount) {
+      this.balance -= amount;
+    }
+  }
+
+  getBalance() {
+    return this.balance;
+  }
+
+  displayAccountInfo() {
+    console.log(`Nom du Compte: ${this.accountName}`);
+    console.log(`Depositire de: ${this.balance}€`);
+    console.log("Apparteant à:");
+    this.clients.forEach(client => {
+      console.log(`  Nom du client: ${client.name}`);
+    });
+  }
 }
 
 export class Client {
-
   constructor(name, balance) {
     this.name = name;
     this.balance = balance;
   }
 
-  deposit(amount) {}
+  deposit(amount) {
+    if (amount > 0) {
+      this.balance += amount;
+      console.log(`Le client ${this.name} a déposé :`, amount , "€")
+    }
+  }
 
-  withdraw(amount) {}
+  withdraw(amount) {
+    if (amount > 0 && this.balance >= amount) {
+      this.balance -= amount;
+      console.log(`Le client ${this.name} a retiré :`, amount , "€")
+    }
+  }
 
-  getBalance() {}
+  getBalance() {
+    return this.balance;
+  }
 
-  displayClientIdentity() {}
+  displayClientIdentity() {
+    console.log(`Nom du client: ${this.name}, Dépositaire: ${this.balance} €`);
+  }
 
-  updateClientIdentity(name) {}
-    
+  updateClientIdentity(name) {
+    console.log(`Le compte de: ${this.name}`);
+    this.name = name;
+    console.log(`appartient désormais à : ${this.name}`);
+  }
 }
--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-import { Bank } from './Bank.js';
-console.log('Hello World');
+import { Bank, Client, CommonAccount } from './Bank.js';
 
+const bank = new Bank();
 
-const bank = new Bank("My Bank");
-console.log(bank.name);
+const client1 = new Client("Marvin", 1000);
+const client2 = new Client("Axel", 1500);
+const client3 = new Client("Matthieu", 1100);
+const client4 = new Client("Matteo", 2500);
+const client5 = new Client("Bastien", 4800);
+
+bank.addClient(client1);
+bank.addClient(client2);
+bank.addClient(client3);
+bank.addClient(client4);
+bank.addClient(client5);
+
+client1.deposit(500);
+client2.withdraw(200);
+
+bank.displayAllClients();
+bank.displayRichestClient(1);
+
+client1.updateClientIdentity("Geoffroy");
+bank.displayClient("Geoffroy");
+
+client1.deposit(5500);
+client2.withdraw(200);
+
+const commonAccount = new CommonAccount("Axel et Bastien", client2, client5);
+
+bank.removeClient(client5);
+
+commonAccount.displayAccountInfo();
+bank.displayRichestClient(5);
