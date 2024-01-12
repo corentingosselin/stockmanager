256ea5be968a15c85d419480184eca8b2b4a836b
Bastien-Lienhard <bastien.lienhard@supinfo.com>
2024-01-12 13:54:25 +0100

    Dog Bastien

--- dog/dog.js
+++ dog/dog.js
@@ -10,4 +10,20 @@
 Ò
  */
 
-    
+function setupDogPic() {
+    const button = document.getElementById('ex_02_btn');
+    const dogImage = document.getElementById('ex_02_img');
+
+    button.addEventListener('click', function () {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json())
+            .then(data => {
+                dogImage.src = data.message;
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération de l\'image du chien:', error);
+            });
+    });
+}
+
+window.onload = setupDogPic;
