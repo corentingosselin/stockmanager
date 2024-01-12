c3eb1ff8362922ae199e22ad193b6ddef7079cf2
nei1stshooter <nao.trambouze@supinfo.com>
2024-01-13 00:16:17 +0900

    Update game.js

--- miner/game.js
+++ miner/game.js
@@ -4,14 +4,19 @@ document.onreadystatechange = function () {
   }
   let score = 0;
   let pickale_pow = 1;
-  let miner = 1;
+  let miner = 0;
+  window.setInterval(function() {
+    score += miner;
+    ingot.textContent = "Ingot : " + String(score);
+  }, 1000)
 
-  const gained_ore = document.querySelector("section > p");
+  const ingot = document.querySelector("section > p");
+  const num_miner = document.querySelector("p + p");
 
   const ore = document.getElementById("ore");
   ore.addEventListener("click", function() {
     score += pickale_pow;
-    gained_ore.textContent = "Ore : " + String(score);
+    ingot.textContent = "Ingot : " + String(score);
   })
 
   const pow_up = document.getElementById("pow_up");
@@ -20,7 +25,7 @@ document.onreadystatechange = function () {
       pickale_pow += 1;
       score -= 10;
     }
-    gained_ore.textContent = "Ore : " + String(score);
+    ingot.textContent = "Ingot : " + String(score);
   })
 
   const add_miner = document.getElementById("add_miner");
@@ -29,7 +34,8 @@ document.onreadystatechange = function () {
       miner += 1;
       score -= 10;
     }
-    gained_ore.textContent = "Ore : " + String(score);
+    ingot.textContent = "Ingot : " + String(score);
+    num_miner.textContent = "Miner : " + String(miner);
   })
 };
 
