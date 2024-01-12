249b5dd5d58c45aca96b46cc8211da28d991d061
ProdigyMan <corentingosselin@hotmail.fr>
2024-01-12 10:27:17 +0100

    correction exo 1

--- promises/promises_1.js
+++ promises/promises_1.js
@@ -1,33 +1,39 @@
-function makeTransaction(amount) {
+async function makeTransaction(amount) {
   console.log("Transaction started");
-  return setTimeout(() => {
-
-    //La transaction a 50% de chance de réussir
-    let random = Math.floor(Math.random() * 100);
-    if (random > 50) {
-      console.log("Transaction failed");
-      return;
-    }
-
-    console.log("Transaction done with amount: " + amount + " €");
-
-  }, 1000);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //La transaction a 50% de chance de réussir
+      let random = Math.floor(Math.random() * 100);
+      if (random > 50) {
+        console.log("Transaction failed");
+        reject("Transaction failed");
+        return;
+      }
+      resolve("Transaction done with amount: " + amount + " €");
+    }, 1000);
+  });
 }
 
-function sendMail(message) {
-  return setTimeout(() => {
-    console.log("Mail sent with message: " + message);
-  }, 1000);
+async function sendMail(message) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Mail sent with message: " + message);
+      resolve();
+    }, 1000);
+  });
 }
 
 console.log("Purchase started");
 
-makeTransaction(100);
-sendMail("email de confirmation");
+makeTransaction(100).then((message) => {
+  console.log(message);
+  sendMail("The transaction is a success");
+}).catch((error) => {
+  console.log(error);
+});
 
-console.log("Bye Bye !");
-
+console.log("Purchase done");
 
 // 1 - exécuter le code et observer le résultat, le comportement est-il celui attendu ?
-// 2 - tenter de corriger le code pour que le comportement soit celui attendu
-// 3 l'email doit être envoyé uniquement si la transaction a réussi
+// 2 - tenter de corrigé le code pour que le comportement soit celui attendu
+// 3 l'email doit être envoyé après la transaction, si la transaction échoue, l'email ne doit pas être envoyé
