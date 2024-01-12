91c8c52d3522ddb7624284441ba57c3ec821fd7e
Bastien-Lienhard <bastien.lienhard@supinfo.com>
2024-01-12 13:54:32 +0100

    Promises

--- promises/promises_1.js
+++ promises/promises_1.js
@@ -1,33 +1,41 @@
 function makeTransaction(amount) {
   console.log("Transaction started");
-  return setTimeout(() => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // La transaction a 50% de chance de réussir
+      let random = Math.floor(Math.random() * 100);
+      if (random > 50) {
+        console.log("Transaction failed");
+        reject("Transaction failed");
+        return;
+      } else {
+        console.log("Transaction done with amount: " + amount + " €");
+        resolve(amount);
+      }
 
-    //La transaction a 50% de chance de réussir
-    let random = Math.floor(Math.random() * 100);
-    if (random > 50) {
-      console.log("Transaction failed");
-      return;
-    }
-
-    console.log("Transaction done with amount: " + amount + " €");
-
-  }, 1000);
+    }, 1000);
+  });
 }
 
 function sendMail(message) {
-  return setTimeout(() => {
-    console.log("Mail sent with message: " + message);
-  }, 1000);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Mail sent with message: " + message);
+      resolve(message);
+    }, 1000);
+  });
 }
 
 console.log("Purchase started");
 
-makeTransaction(100);
-sendMail("Purchase done");
-
-console.log("Purchase done");
-
+makeTransaction(100)
+  .then((amount) => {
+    sendMail("Purchase done");
+  })
+  .catch((error) => {
+    console.error(error.message);
+  })
 
 // 1 - exécuter le code et observer le résultat, le comportement est-il celui attendu ?
 // 2 - tenter de corriger le code pour que le comportement soit celui attendu
-// 3 l'email doit être envoyé après la transaction, si la transaction échoue, l'email ne doit pas être envoyé
+// 3 - l'email doit être envoyé après la transaction, si la transaction échoue, l'email ne doit pas être envoyé
--- promises/promises_2.js
+++ promises/promises_2.js
@@ -1,3 +1,54 @@
-// reprendre l'exercice promises/promises_1.js
 // Cette fois-ci nous devons réaliser 5 transactions en même temps et envoyer un email si toutes les transactions sont réussies.
 // Vous pouvez modifier le pourcentage de chance de réussite de la transaction pour tester votre code.
+
+
+function makeTransaction(amount) {
+    console.log("Transaction started");
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // La transaction a 50% de chance de réussir
+        let random = 0;
+        if (random > 50) {
+          console.log("Transaction failed");
+          reject("Transaction failed");
+          return;
+        } else {
+          console.log("Transaction done with amount: " + amount + " €");
+          resolve(amount);
+        }
+  
+      }, 1000);
+    });
+  }
+  
+  function sendMail(message) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("Mail sent with message: " + message);
+        resolve(message);
+      }, 1000);
+    });
+  }
+  
+  console.log("Purchase started");
+
+  const transactionPromises = [];
+  
+  for (let i = 1; i <= 5; i++) {
+    const transactionPromise = makeTransaction(100 * i);
+    transactionPromises.push(transactionPromise);
+  }
+  
+  Promise.all(transactionPromises)
+    .then((amounts) => {
+      return sendMail("Purchase done with amounts: " + amounts.join(" €, ") + " €");
+    })
+    .then((message) => {
+      console.log(message);
+    })
+    .catch((error) => {
+      console.error(error.message);
+    })
+    .finally(() => {
+      console.log("Purchase done");
+    });
