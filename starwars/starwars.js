346b35fcb0595906994cbe09fd0a0fd99f2d41b1
Bastien-Lienhard <bastien.lienhard@supinfo.com>
2024-01-12 13:54:01 +0100

    Star Wars Bastien

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -17,3 +17,43 @@ url api: https://swapi.dev/
 endpoint: https://swapi.dev/api/people/?page=1
 */
 
+document.addEventListener('DOMContentLoaded', function () {
+   var currentPage = 1;
+ 
+   function getAndDisplayCharacters(page) {
+     fetch(`https://swapi.dev/api/people/?page=${page}`)
+       .then(response => {
+         if (!response.ok) {
+           throw new Error(`Erreur`);
+         }
+         return response.json();
+       })
+       .then(data => {
+         var contentElement = document.getElementById('content');
+         contentElement.innerHTML = '';
+ 
+         data.results.forEach(function (character) {
+           var h3 = document.createElement('h3');
+           h3.textContent = character.name;
+           contentElement.appendChild(h3);
+         });
+ 
+         var nextPageButton = document.getElementById('next-page-button');
+         if (data.next) {
+           nextPageButton.style.display = 'block';
+         } else {
+           nextPageButton.style.display = 'none';
+         }
+       })
+       .catch(error => {
+         console.error(error);
+       });
+   }
+ 
+   document.getElementById('next-page-button').addEventListener('click', function () {
+     currentPage++;
+     getAndDisplayCharacters(currentPage);
+   });
+ 
+   getAndDisplayCharacters(currentPage);
+ });
